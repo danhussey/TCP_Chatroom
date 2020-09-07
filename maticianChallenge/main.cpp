273375f807beec5c6eb04e75997e1e678143dd5a
Daniel <daniel.r.hussey@gmail.com>
2020-09-07 22:15:23 +1000

    Initial Commit

---
+++ maticianChallenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maticianChallenge
+//
+//  Created by Daniel Hussey on 7/9/20.
+//  Copyright © 2020 Daniel Husey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
